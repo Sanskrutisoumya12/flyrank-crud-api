920abe6cef7d5125f9f044bdea7919fe797286aa
Sanskruti Soumya Nayak <sanskrutinayak39@gmail.com>
2026-07-29 19:02:28 +0530

    Stage 5: added delete task endpoint

--- main.py
+++ main.py
@@ -78,3 +78,18 @@ def update_task(task_id: int, updated_task: Task):
         status_code=404,
         detail="Task not found"
     )
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {
+                "message": "Task deleted successfully"
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
