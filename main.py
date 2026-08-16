9ef3fa3e33637d316aed3f8ac839bb80282a58bc
Sanskruti Soumya Nayak <sanskrutinayak39@gmail.com>
2026-08-16 19:37:29 +0530

    Stage 4: update and delete with SQL

--- main.py
+++ main.py
@@ -67,29 +67,38 @@ def create_task(task: Task):
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, updated_task: Task):
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
 
-    for task in tasks:
-        if task["id"] == task_id:
-            task["title"] = updated_task.title
-            task["completed"] = updated_task.completed
-            return task
+        if not task:
+            raise HTTPException(
+                status_code=404,
+                detail="Task not found"
+            )
 
-    raise HTTPException(
-        status_code=404,
-        detail="Task not found"
-    )
+        task.title = updated_task.title
+        task.completed = updated_task.completed
+
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+
+        return task
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
 
-    for task in tasks:
-        if task["id"] == task_id:
-            tasks.remove(task)
-            return {
-                "message": "Task deleted successfully"
-            }
+        if not task:
+            raise HTTPException(
+                status_code=404,
+                detail="Task not found"
+            )
 
-    raise HTTPException(
-        status_code=404,
-        detail="Task not found"
-    )
+        session.delete(task)
+        session.commit()
+
+        return {
+            "message": "Task deleted successfully"
+        }
